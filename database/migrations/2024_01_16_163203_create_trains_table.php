b25d2d533c2fd7f5710466d9ee795ee229475ac7
lexbert17 <lexbertlazo@gmail.com>
2024-01-16 18:21:16 +0100

    add:creazione migration

---
+++ database/migrations/2024_01_16_163203_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company');
+            $table->string('from');
+            $table->string('to');
+            $table->date('date');
+            $table->time('departure time');
+            $table->time('arrival time');
+            $table->string('train code');
+            $table->tinyInteger('number of carriaages');
+            $table->boolean('delay');
+            $table->boolean('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
